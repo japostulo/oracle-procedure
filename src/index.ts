8f0ccc84771f3f69fcf3ad6cb36db2ce238761b5
João Apostulo Neto <japostulo@gmail.com>
2023-03-31 14:32:16 -0300

    Adiciona exportação de todos os recursos disponíveis em index

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,4 @@
-import { DataSource } from "typeorm";
-import { APIConsultaItemPepOnline } from "./api-consulta-item-pep-online";
-import { UppercaseNamingStrategy } from "./UppercaseNamingStrategy";
-
-const connection = new DataSource({
-  type: "oracle",
-  database: "tasy",
-  username: "tasy",
-  password: "haoc_teste",
-  connectString:
-    "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=scan-dbprojeto1.oswaldocruz.intranet)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=dbprojeto1.oswaldocruz.intranet)(FAILOVER_MODE=(TYPE=select)(METHOD=basic))))",
-  port: 1521,
-  logging: true,
-  namingStrategy: new UppercaseNamingStrategy(),
-});
-
-async function connect() {
-  await connection.initialize();
-
-  const procedure = new APIConsultaItemPepOnline(connection);
-
-  procedure.cd_pessoa_fisica = "366630";
-  procedure.nr_atendimento = 4394417;
-  procedure.item = "VA";
-  procedure.opcao_retorno = "DADOS";
-
-  await procedure.execute();
-}
-
-connect();
+export * from "./decorator/bind-in";
+export * from "./decorator/bind-out";
+export * from "./Procedure";
+export * from "./UppercaseNamingStrategy";
